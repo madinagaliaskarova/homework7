b8fb4e9e122610bae9e0da5716f2131c7ec81d54
KingAli <a.tursunov1210@gmail.com>
2022-03-19 23:55:39 +0600

    home work7

---
+++ src/com/company/Magic.java
@@ -0,0 +1,14 @@
+
+package com.company;
+
+public class Magic extends Hero {
+
+    public Magic(String superAbility) {
+        super(superAbility);
+    }
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Magic applied " + getSuperAbility() + " super ability");
+    }
+}
